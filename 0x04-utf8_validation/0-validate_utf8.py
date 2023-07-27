80eb33351bab00466f7cca0c8e3f3d22ea6ee7c2
OtaraAlex <mbechealex61@gmail.com>
2023-07-27 01:04:50 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,25 +4,25 @@ UTF-8 Validation
 """
 
 
-def validUTF8(data):
+def validUTF8(data) -> bool:
     """
-    Data: a list of integers
-    Return: True if data is a valid UTF-8
-    encoding, else return False
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    byte_count = 0
-
-    for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if byte >> 6 != 0b10:
                 return False
-            byte_count -= 1
+        num_bytes -= 1
+    return num_bytes == 0
